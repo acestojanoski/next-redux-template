6e95d8299826578c8f6ab392f3f99fa1eedb42d7
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-07-02 00:29:35 +0200

    redux api loading status

--- src/api/api-reducers.js
+++ src/api/api-reducers.js
@@ -3,6 +3,9 @@ import {fromJS} from 'immutable';
 export const API_CALL_STARTED = (state, action) => {
     const status = {
         timestamp: action.timestamp,
+        isLoading: true,
+        isLoaded: false,
+        isError: false,
         request: action.request
     };
 
@@ -12,6 +15,9 @@ export const API_CALL_STARTED = (state, action) => {
 export const API_CALL_FINISHED = (state, action) => {
     const status = {
         timestamp: action.timestamp,
+        isLoading: false,
+        isLoaded: true,
+        isError: false,
         ...action.response
     };
 
@@ -21,6 +27,9 @@ export const API_CALL_FINISHED = (state, action) => {
 export const API_CALL_FAILED = (state, action) => {
     const status = {
         timestamp: action.timestamp,
+        isLoading: false,
+        isLoaded: false,
+        isError: true,
         ...action.error
     };
 
